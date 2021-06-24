ffb275cd6e361250f075a26145abb0b9fd89a153
nn <nicholasnadel@gmail.com>
2021-06-24 16:18:28 -0700

    progress with mobile switcheroo
    slate embed makes it difficult to use a separate/second markup

--- custom.js
+++ custom.js
@@ -1,9 +1,30 @@
-var elementPosition = $(".slate-form__wrapper").offset();
+$(function () {
+  var elementPosition = $(".slate-form__wrapper").offset();
 
-$(window).scroll(function () {
-  if ($(window).scrollTop() > elementPosition.top) {
-    $(".slate-form__wrapper").attr("data-sticky", "true");
-  } else {
-    $(".slate-form__wrapper").attr("data-sticky", "false");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > elementPosition.top) {
+      $(".slate-form__wrapper").attr("data-sticky", "true");
+    } else {
+      $(".slate-form__wrapper").attr("data-sticky", "false");
+    }
+  });
+
+  $(window).on("load resize", function (e) {
+    moveSlateform();
+  });
+
+  function moveSlateform() {
+    isMobile = $(".mobile-only").is(":visible");
+    if (isMobile) {
+      console.log("mobile view");
+      $(".slate-form__wrapper").detach().appendTo(".mobile-only");
+    } else {
+      $(".slate-form__wrapper").detach().insertAfter(".cu-masthead__wrapper");
+    }
+    // if ($(window).innerWidth() <= 1024) {
+    //   $(".slate-form__wrapper").detach().appendTo(".testimonial__wrapper");
+    // } else {
+    //   $(".slate-form__wrapper").detach().appendTo(".cu-masthead__wrapper");
+    // }
   }
 });
