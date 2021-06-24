a00a173473d98ca5e49eda621bbafb5789eda1a6
nn <nicholasnadel@gmail.com>
2021-06-24 13:29:49 -0700

    form - use position: sticky instead of fixed

--- custom.js
+++ custom.js
@@ -1,9 +1,9 @@
-var elementPosition = $('.slate-form__wrapper').offset();
+var elementPosition = $(".slate-form__wrapper").offset();
 
-$(window).scroll(function(){
-        if($(window).scrollTop() > elementPosition.top){
-              $('.slate-form__wrapper').attr('data-fixed', 'true');
-        } else {
-            $('.slate-form__wrapper').attr('data-fixed', 'false');
-        }    
+$(window).scroll(function () {
+  if ($(window).scrollTop() > elementPosition.top) {
+    $(".slate-form__wrapper").attr("data-sticky", "true");
+  } else {
+    $(".slate-form__wrapper").attr("data-sticky", "false");
+  }
 });
