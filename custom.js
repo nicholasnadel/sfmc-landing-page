11472ac2c0c201e7a5d165b0f4663887959ef3e5
nn <nicholasnadel@gmail.com>
2021-06-24 13:20:50 -0700

    basic slate form

--- custom.js
+++ custom.js
@@ -1,21 +1,9 @@
-// javascipt
-function sticky_relocate() {
-  console.log("shalom ");
-  var window_top = $(window).scrollTop();
-  var footer_top = $("#footer").offset().top;
-  var div_top = $(".smc-lp__text-conatiner").offset().top;
-  var div_height = $(".smc-lp__form-container").height();
+var elementPosition = $('.slate-form__wrapper').offset();
 
-  if (window_top + div_height > footer_top)
-    $(".smc-lp__form-container").removeClass("sticky");
-  else if (window_top > div_top) {
-    $(".smc-lp__form-container").addClass("sticky");
-  } else {
-    $(".smc-lp__form-container").removeClass("sticky");
-  }
-}
-
-$(function () {
-  $(window).scroll(sticky_relocate);
-  sticky_relocate();
+$(window).scroll(function(){
+        if($(window).scrollTop() > elementPosition.top){
+              $('.slate-form__wrapper').attr('data-fixed', 'true');
+        } else {
+            $('.slate-form__wrapper').attr('data-fixed', 'false');
+        }    
 });
