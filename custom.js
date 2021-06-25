df10b3ffe3347a2df641cd489826c8d0fe297d4a
nn <nicholasnadel@gmail.com>
2021-06-25 12:06:17 -0700

    check that window resize is not triggered by mobile keyboard https://stackoverflow.com/questions/14902321/how-to-determine-if-a-resize-event-was-triggered-by-soft-keyboard-in-mobile-brow

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
 $(function () {
+  var isMobile = $(".mobile-only").is(":visible");
   var elementPosition = $(".slate-form__wrapper").offset();
   var offset = $(".bottom-cta").outerHeight();
   var topofDiv = $(".bottom-cta").offset().top;
@@ -11,16 +12,26 @@ $(function () {
     }
   });
 
-  $(window).on("resize load", function (e) {
+  $(window).on("load", function (e) {
     moveSlateform();
   });
 
+  $(window).on("resize", function () {
+    // If the current active element is a text input, we can assume the soft keyboard is visible.
+    if ($(document.activeElement).prop("type") === "text") {
+      // Logic for while keyboard is shown
+      return;
+    } else {
+      moveSlateform();
+    }
+  });
+
   function moveSlateform() {
-    var isMobile = $(".mobile-only").is(":visible");
     if (isMobile) {
       $(".slate-form__wrapper").detach().appendTo(".mobile-only");
       $(".slate-form__wrapper").attr("data-sticky", "false");
       $(".slate-form__wrapper").css("margin-bottom", "unset");
+      $(".slate-form__wrapper").css("all", "initial");
     } else {
       $(".slate-form__wrapper").detach().insertAfter(".cu-masthead__wrapper");
       if ($(window).scrollTop() > topofDiv + offset) {
